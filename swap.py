0f81d11b696f43a5c953e6c2702bccd21965aab1
Tanishkagit <144674434+Tanishkagit@users.noreply.github.com>
2023-10-13 21:42:19 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+number1=int(input("enter the number1:- "))
+number2=int(input("enter the number2:- "))
+number1=number1+number2
+number2=number1-number2
+number1=number1-number2
+print(number1)
+print(number2)
