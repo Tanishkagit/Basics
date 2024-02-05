fdc900b5e67c6a872c056f058e074618657a2098
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-02-05 22:43:40 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -5,3 +5,12 @@ number2=number1-number2
 number1=number1-number2
 print(number1)
 print(number2)
+
+//using bitwise operator.
+number1=int(input("enter the number1:- "))
+number2=int(input("enter the number2:- "))
+number1=number1^number2
+number2=number1^number2
+number1=number1^number2
+print(number1)
+print(number2)
