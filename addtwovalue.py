db8297be366de61eda60dd9ea7ce4ff2f43aa41e
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-02-06 22:09:25 +0530

    Update addtwovalue.py

--- addtwovalue.py
+++ addtwovalue.py
@@ -2,3 +2,16 @@ num1=int(input("enter num1 value:- "))
 num2=int(input("enter num2 value:- "))
 num=num1+num2
 print(num)
+
+//....
+num1=6
+num2=19
+print(num1+num2)
+
+//.....
+num1=21
+num2=12
+num=num1+num2
+print(num)
+
+
