f0786ab94bace40f80dd942962c7255544863e54
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-02-12 21:09:46 +0530

    Create swappingvalues.py

---
+++ swappingvalues.py
@@ -0,0 +1,24 @@
+#write a program swap two value without using third variable.
+a=10
+b=8
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+#write a program swap two value with using third variable.
+a=10
+b=8
+c=a
+a=b
+b=c
+print(a)
+print(b)
+#write a program swap two value  using bitwise OR.
+a=10
+b=8
+a=a^b
+b=a^b
+a=a^b
+print(a)
+print(b)
