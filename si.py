5fb3837ef22ac973f0f54f471e23a13e99ad9d72
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-04-03 20:49:15 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,5 +1,5 @@
-P=int(input("enter the P:- "))
-R=int(input("enter the R:- "))
-T=int(input("enter the T:- "))
-simpleinterest=P*R*T/100
+Profit=int(input("enter the P:- "))
+Rate=int(input("enter the R:- "))
+Time=int(input("enter the T:- "))
+simpleinterest=Profit*Rate*Time/100
 print(simpleinterest)
